70f210c0a62ab36b86e8843517b0e9c857579166
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-02-03 19:01:52 +0100

    Add the user model, jsonwebtoken configuration and the error handler

---
+++ src/utils/appError.ts
@@ -0,0 +1,13 @@
+export default class AppEror extends Error {
+    status: string;
+    isOperational: boolean;
+    contructor: Function | undefined;
+
+    constructor(public message: string, public statusCode: number=500) {
+        super(message);
+        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+        this.isOperational = true;
+
+        Error.captureStackTrace(this, this.contructor);
+    }
+}
--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose';
 import config from 'config';
 
-const dbUrl = `mongodb://${config.get('dbName')}:${config.get('dbPass')}@localhost:6000/typescript-auth-api?authSource=asmin`;
+const dbUrl = `mongodb://${config.get('dbName')}:${config.get('dbPass')}@localhost:6000/typescript-auth-api?authSource=admin`;
 
 const connectDB = async () => {
     try {
+        mongoose.set('strictQuery', false);
         await mongoose.connect(dbUrl);
         console.log('Database Connected...');
     } catch (err: any) {
