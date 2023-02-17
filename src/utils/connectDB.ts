7f9cf0319fae467f000938f47fb776fd1e5f5a88
Oluwatimilehin Oreoluwa Idowu <oluwatimilehin.id@gmail.com>
2023-02-18 00:25:01 +0100

    Reconfigure the mongodb url

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import config from 'config';
 
-const dbUrl = `${config.get('dbUrl')}`;
+const dbUrl: string = config.get('dbUrl');
 
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        await mongoose.connect(dbUrl);
+        mongoose.connect(dbUrl);
         console.log('Database Connected...');
     } catch (err: any) {
         console.log(err.message);
